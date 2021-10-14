c4aa54d3b022ecd0c9a8f6bfdc7191ea51a06cb0
Caedmon Evans <cevan828@students.bju.edu>
2021-10-14 11:43:47 -0400

    updates

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, Response, redirect, send_from_directory
+from flask import Flask, request, Response, redirect, send_from_directory, abort
+from db.game import Game
+from db.player import Player
 from htmllib import html, head, body, div, h1, div
 from db.util import gen_id
 from random import random
+import json
 
 app = Flask(__name__)
 
@@ -60,24 +63,86 @@ def join_game():
     resp = Response()
 
     # if not a registered user
-    if request.cookies.get("user_id") is None:
-        resp.set_cookie("user_id", gen_id())
+    user_id = request.cookies.get("user_id")
+    if user_id is None:
+        new_player = Player()
+        user_id = gen_id()
+        resp.set_cookie("user_id", new_player.id)
 
+    # get game_id if present
     game_id = request.args.get("id")
-    if game_id is None:
-        game_id = gen_id()
 
-    resp.location = f"/play-game?id={game_id}"
-    resp.status_code = 302
+    # if given a game id
+    if game_id:
+        game = Game.get(game_id)
+
+        # if the game exists
+        if game:
+            
+            # if the user successfully joined the game
+            if game.join(user_id):
+                print("joining game", game_id)
+                resp.location = f"/play-game?id={game_id}"
+                resp.status_code = 302
+                resp.set_data("{}")
+            
+            # if the user failed to join the game
+            else:
+                print("user could not join game", game_id)
+                resp.set_data("{}")
+
+        # if the game does not exist
+        else:
+            print(f"game {game_id} does not exist")
+            resp.set_data(json.dumps({
+                "error": "game does not exist" 
+            }))
+    
+    # if there is another player in the queue
+    elif len(game_queue) > 0 and user_id not in game_queue:
+
+        print("creating a new game")
+        
+        # create a new game
+        game = Game()
+        game.join(user_id)
+        game.join(game_queue.pop())
+
+        resp.location = f"/play-game?id={game.id}"
+        resp.status_code = 302
+        resp.set_data("{}")
+
+    else:
+        print("adding player to queue", len(game_queue))
+        game_queue.add(user_id)
+        resp.set_data("{}")
 
     # return redirect("/play-game?id=", resp)
     return resp
 
+@app.route("/queue-status")
+def queue_status():
+    player = Player.get(request.cookies.get("user_id"))
+    if not player:
+        return abort(401)
+    
+    game_id = player.get_next_game()
+    if not game_id:
+        return {}
+    
+    return redirect(f"/player-game?id={game_id}")
+    
+
 @app.route("/play-game")
 def play_game():
-    game_id = request.args.get("id")
+
+    # get game
+    game = Game.get(request.args.get("id"))
+    if not game:
+        return abort(404)
+
     return base(
-        h1(f"Game {game_id}"),
+        h1(f"Game {game.id}"),
         create_game_board()
     )
 
